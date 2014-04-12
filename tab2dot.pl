a40a158fbc2607a3e2608f58b3419208430a597e
Diomidis Spinellis <dds@aueb.gr>
2014-04-12 14:29:03 +0300

    Implement automatic folding

--- tab2dot.pl
+++ tab2dot.pl
@@ -1,29 +1,58 @@
 #!/bin/env perl
 
+use strict;
+use warnings;
+use open qw(:std :utf8);
+
+binmode STDOUT, ':utf8';
+
+# Maximum line length (in characters)
+my $line_length = 20;
+
 print q(digraph D {
 	splines=ortho;
 	ranksep=0.2;
-	node [shape=box, width=3];
+	node [shape=box, width=2];
 	edge [dir=none];
 	randir = TB;
 );
 
+# Close a dot cluster structure
 sub
 close_cluster
 {
 	print ";\n\t}\n";
 }
 
+# Fold the text on the specified width
+sub
+fold_text
+{
+	my ($text) = @_;
+	my $result = '';
+	while (length($text) > $line_length) {
+		if (!($text =~ s/^(.{3,20}) (.*)/$2/)) {
+			print STDERR "Unable to fold $text\n";
+			exit 1;
+		}
+		$result .= "$1\\n";
+	}
+	$result .= $text;
+	return $result;
+}
+
+
 my $cluster;
+my $top;
 
 while (<>) {
 	# Ignore line comments
 	next if (/^\s*\#/);
 
 	# Parse input line
-	m/^(\t*)(.*)\n/;
-	$level = length($1);
-	$name = $2;
+	m/^(\t*)([^\r\n]*)\r?\n/;
+	my $level = length($1);
+	my $name = fold_text($2);
 
 	# Rules for each diagram level
 	if ($level == 0) {
