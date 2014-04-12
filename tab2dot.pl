b6f73f9cec1599c22094f7222388cdb172132b39
Diomidis Spinellis <dds@aueb.gr>
2014-04-12 14:00:49 +0300

    Convert diagram into cluster-based
    This works only for three-layer diagrams, but produces much neater
    results.

--- tab2dot.pl
+++ tab2dot.pl
@@ -1,15 +1,42 @@
+#!/bin/env perl
+
 print q(digraph D {
+	splines=ortho;
+	ranksep=0.2;
+	node [shape=box, width=3];
+	edge [dir=none];
 	randir = TB;
 );
+
+sub
+close_cluster
+{
+	print ";\n\t}\n";
+}
+
+my $cluster;
+
 while (<>) {
 	m/^(\t*)(.*)\n/;
-	$this_level = length($1);
-	$this_name = $2;
-	$name[$this_level] = $this_name;
-	my $parent_name;
-	if ($this_level > 0) {
-		$parent_name = $name[$this_level - 1];
-		print qq{\t"$parent_name" -> "$this_name";\n};
+	$level = length($1);
+	$name = $2;
+	if ($level == 0) {
+		$top = $name;
+	} elsif ($level == 1) {
+		close_cluster if (defined($cluster));
+		$cluster++;
+		print qq(
+	"$top" -> "$name";
+	subgraph cluster_$cluster {
+		color=lightgray;
+		style=filled;
+		edge [style=invis];
+		"$name" [shape=plaintext];
+		node [shape=box, color=white, style=filled];
+		"$name" );
+	} elsif ($level == 2) {
+		print qq( -> "$name" );
 	}
 }
+close_cluster;
 print "}\n";
